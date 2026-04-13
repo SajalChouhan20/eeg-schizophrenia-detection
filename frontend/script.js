be343217eb9d2b965b378f7df69c20c79c67cbf8
Sajal Chouhan <chouhansajal9@gmail.com>
2026-04-13 16:26:21 +0530

    Initial commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("predictBtn")
-        .addEventListener("click", function(event) {
+        .addEventListener("click", function (event) {
             event.preventDefault();
             uploadFile();
         });
@@ -11,18 +11,20 @@ document.addEventListener("DOMContentLoaded", function () {
 async function uploadFile() {
     const fileInput = document.getElementById("fileInput");
     const resultText = document.getElementById("result");
+    const loader = document.getElementById("loader");
 
     const file = fileInput.files[0];
 
     if (!file) {
-        alert("Upload file first!");
+        alert("Please upload a file first!");
         return;
     }
 
     const formData = new FormData();
     formData.append("file", file);
 
-    resultText.innerText = "Processing...";
+    resultText.innerText = "";
+    loader.style.display = "block";
 
     try {
         const response = await fetch("http://127.0.0.1:5000/predict", {
@@ -32,9 +34,20 @@ async function uploadFile() {
 
         const data = await response.json();
 
+        loader.style.display = "none";
+
         resultText.innerText = data.prediction || data.error;
 
+        // color feedback
+        if (data.prediction) {
+            resultText.style.color = "lime";
+        } else {
+            resultText.style.color = "red";
+        }
+
     } catch (error) {
+        loader.style.display = "none";
         resultText.innerText = "Server Error!";
+        resultText.style.color = "red";
     }
 }
